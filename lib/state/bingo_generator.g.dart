9ac300bd69f05234f5b042fec12b10e48f3f8091
rmc-8.com <kmprmc@gmail.com>
2024-07-16 14:51:36 +0900

    feat: Implement Bingo Card with Riverpod

---
+++ lib/state/bingo_generator.g.dart
@@ -0,0 +1,26 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'bingo_generator.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$bingoGeneratorHash() => r'a17df8ac3d4243fd7ed30460351ea0b2b8b8d203';
+
+/// See also [BingoGenerator].
+@ProviderFor(BingoGenerator)
+final bingoGeneratorProvider =
+    AutoDisposeNotifierProvider<BingoGenerator, List<int>>.internal(
+  BingoGenerator.new,
+  name: r'bingoGeneratorProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$bingoGeneratorHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$BingoGenerator = AutoDisposeNotifier<List<int>>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
