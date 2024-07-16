1e11ed7f87c4122e31021d735516cc3c3b9edfd1
rmc-8.com <kmprmc@gmail.com>
2024-07-16 13:47:48 +0900

    Init

---
+++ android/app/src/main/kotlin/com/example/riverpod_practice_bingo/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.riverpod_practice_bingo
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const BingoApp());
+}
+
+class BingoApp extends StatelessWidget {
+  const BingoApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Center(
+            child: Text("Bingo Game"),
+          ),
+        ),
+        body: Container(),
+      ),
+    );
+  }
+}
